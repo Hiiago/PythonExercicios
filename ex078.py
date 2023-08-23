cf4ced09d58f3cf1a9ddb9c201762f26984727fb
Hiago <114781934+Hiiago@users.noreply.github.com>
2023-08-23 15:27:55 -0300

    Delete ex078.py

--- ex078.py
+++
@@ -1,8 +0,0 @@
-'''
-Elabore um algoritmo que leia os salários de 10 trabalhadores de uma empresa
- e os armazene num vetor. Depois, em ciclos independentes
- Calcule a média desses salários
- Determine o maior dos salários desta empresa
- Conte os salários menores de R$850,00.
-'''
-
