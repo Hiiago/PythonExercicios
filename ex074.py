3059fdcc773951eb440b86a7e8ed2742c20601fe
Hiago <114781934+Hiiago@users.noreply.github.com>
2023-08-31 14:02:56 -0300

    Update ex074.py

--- ex074.py
+++ ex074.py
@@ -3,16 +3,13 @@ Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla
  Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
 '''
 
-import random
+from random import randint
 
+numeros = (randint(1, 10), randint(1, 10), randint(1, 10),
+           randint(1, 10), randint(1, 10))
 
-maior = 0
-menor = 0
-
-
-print('Os números sorteados foram: ', end='')
-for c in range(0, 5):
-    numeros = (random.randint(0, 10))
-    print(f'{numeros}', end=' ')
-
-
+print('Os valores sorteados foram: ', end='')
+for n in numeros:
+    print(f'{n} ', end='')
+print(f'\nMaior: {max(numeros)}\n'
+      f'Menor: {min(numeros)}')
